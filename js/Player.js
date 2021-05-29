c39b5a1fa388a97b1d5afafca572d38f4df981e5
ManasGit1013 <78263712+manassavlani@users.noreply.github.com>
2021-05-29 11:27:53 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,47 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+   GetCarsAtEnd() {
+     database.ref('CarsAtEnd').on("value", (data)=>{
+       this.rank = data.val();
+     })
+
+   }
+   static UpdateCarsAtEnd(rank) {
+     database.ref('/').update({
+       CarsAtEnd:rank
+
+     })
+   }
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
